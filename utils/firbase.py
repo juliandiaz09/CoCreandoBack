4bbd63dbdcf98b3b0f25af8804108dff3fb50294
Julian Diaz <julian.diaz09@uptc.edu.co>
2025-05-04 20:21:36 -0500

    Nueva configuración base de datos firbase

---
+++ utils/firbase.py
@@ -0,0 +1,18 @@
+import os
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+import firebase_admin
+from firebase_admin import credentials
+
+# Construir la ruta absoluta al JSON
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+cred_path = os.path.join(BASE_DIR, 'cocreando.json')
+
+# Inicializar Firebase
+cred = credentials.Certificate(cred_path)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+db = firestore.client()
+print("Firebase inicializado correctamente.")
